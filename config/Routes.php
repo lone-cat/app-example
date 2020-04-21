6d6dc060f12ddfc472d1f1d8a35664fcf6b2d1b6
Alex <alex@melentev.net>
2020-04-21 23:54:19 +0300

    init

--- config/Routes.php
+++ config/Routes.php
@@ -2,9 +2,17 @@
 
 use app\HTTP\Controllers\DefaultController;
 use LoneCat\Router\Router;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
 
 $router = $container->get(Router::class);
 
 $router->addGet('main', '/', DefaultController::class);
 
-$router->addGet('page2', '/page2', [DefaultController::class => 'page2']);
+$router->addGet('page2', '/page2', [DefaultController::class => 'page2'])->middleware(
+    function (ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface {
+        $response = $handler->handle($request);
+        return $response->withHeader('NewHeader', 'My new header');
+    }
+);
